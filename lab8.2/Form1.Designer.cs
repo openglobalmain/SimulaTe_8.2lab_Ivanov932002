06ccd0966195214ddcc10b151561152d3abfc193
Danil Ivanov <nokia5230dok@yandex.ru>
2023-05-12 19:31:36 +0700

    Добавьте файлы проекта.

---
+++ lab8.2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace lab8._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtAns = new System.Windows.Forms.TextBox();
+            this.btAnswer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Location = new System.Drawing.Point(347, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Magic Ball";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtAns
+            // 
+            this.txtAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtAns.Location = new System.Drawing.Point(158, 208);
+            this.txtAns.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAns.Name = "txtAns";
+            this.txtAns.Size = new System.Drawing.Size(468, 60);
+            this.txtAns.TabIndex = 5;
+            // 
+            // btAnswer
+            // 
+            this.btAnswer.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btAnswer.Location = new System.Drawing.Point(338, 159);
+            this.btAnswer.Name = "btAnswer";
+            this.btAnswer.Size = new System.Drawing.Size(92, 32);
+            this.btAnswer.TabIndex = 6;
+            this.btAnswer.Text = "Answer";
+            this.btAnswer.UseVisualStyleBackColor = false;
+            this.btAnswer.Click += new System.EventHandler(this.btAnswer_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(335, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ask a question";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btAnswer);
+            this.Controls.Add(this.txtAns);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtAns;
+        private System.Windows.Forms.Button btAnswer;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab8.2/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAnswer_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            double p1 = 0.1;
+            double p2 = p1 + 0.2;
+            double p3 = p2 + 0.15;
+            double p4 = p3 + 0.25;
+            double p5 = p4 + 0.2;
+            double p6 = p5 + 0.1;
+
+            double a = rand.NextDouble();
+
+            if (a < p1)
+            {
+                txtAns.Text = "No";
+            }
+            else if (a < p2)
+            {
+                txtAns.Text = "Doubt";
+            }
+            else if (a < p3)
+            {
+                txtAns.Text = "I don't know";
+            }
+            else if (a < p4)
+            {
+                txtAns.Text = "Probably";
+            }
+            else if (a < p5)
+            {
+                txtAns.Text = "Yes";
+            }
+            else if (a < p6)
+            {
+                txtAns.Text = "For sure!";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
